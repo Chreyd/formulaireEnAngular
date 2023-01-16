b909578478c52cdf83674e49741224287f1895cc
Chreyd <chreydmapeba@gmail.com>
2023-01-16 15:57:59 +0100

    Récuperer les données d'un formulaire

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './user';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  public user : User = new User();
+  public saveData( registerForm: NgForm){
+    console.log(registerForm.form);
+    console.log('object', JSON.stringify(registerForm.value))
+  }
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+  }
 
 }

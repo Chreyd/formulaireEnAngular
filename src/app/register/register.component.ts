8803791f660822b113e82f6d4baaf0e141957d51
Chreyd <chreydmapeba@gmail.com>
2023-01-17 10:36:01 +0100

    Validation en cours d'exécution: setValidators( ), clearValidators( ), updateValidators( )

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,33 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 import { User } from './user';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   public registerForm!: FormGroup;
 
-  public user : User = new User();
-  public saveData( ){
+  public user: User = new User();
+  public saveData() {
     console.log(this.registerForm);
-    console.log('object', JSON.stringify(this.registerForm?.value))
+    console.log('object', JSON.stringify(this.registerForm?.value));
   }
 
-  constructor(private fb : FormBuilder){
+  public setNotificationSetting(method: string): void {
+    const phoneControl = this.registerForm.get('phone');
+
+    if (method == 'texte') {
+      phoneControl?.setValidators(Validators.required);
+    } else {
+      phoneControl?.clearValidators();
+    }
+
+    phoneControl?.updateValueAndValidity();
   }
+
+  constructor(private fb: FormBuilder) {}
   ngOnInit(): void {
-    this.registerForm=this.fb.record({
-      firstName: ['',[Validators.required, Validators.maxLength(10)]],
-      lastName: ['',[Validators.required, Validators.minLength(4)]],
+    this.registerForm = this.fb.record({
+      firstName: ['', [Validators.required, Validators.maxLength(10)]],
+      lastName: ['', [Validators.required, Validators.minLength(4)]],
       email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
       sendCatalog: false,
-    })
+    });
 
-/*     this.registerForm = new FormGroup({
+    /*     this.registerForm = new FormGroup({
       firstName: new FormControl(),
       lastName: new FormControl(),
       email: new FormControl(),
@@ -35,17 +52,18 @@ export class RegisterComponent implements OnInit {
     }); */
   }
 
-  public fillFormData():void{
-    this.registerForm.setValue({
+  public fillFormData(): void {
+    this.registerForm.patchValue({
+      firstName: 'Chreyd',
+      lastName: 'BPloop',
+      email: 'chre@tset.com',
+      sendCatalog: true,
+    });
+    /*     this.registerForm.patchValue({
       firstName: 'Chreyd',
       lastName: 'BPloop',
       email: "chre@tset.com",
       sendCatalog: true,
-    })
-/*     this.registerForm.patchValue({
-      firstName: 'Chreyd',
-      lastName: 'BPloop',
     }) */
   }
-
 }
